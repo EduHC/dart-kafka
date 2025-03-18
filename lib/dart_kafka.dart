f007bc651fa1cd26e2b9aa5346f820ba4951bb11
Eduardo Horn Cicchella <euardo.cic.horn@gmail.com>
2025-03-18 00:06:29 -0300

    Removing comments from the main library class

--- lib/dart_kafka.dart
+++ lib/dart_kafka.dart
@@ -1,11 +1,9 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
 export 'src/kafka_admin.dart';
 export 'src/kafka_client.dart';
 export 'src/kafka_producer.dart';
-export 'src/kafka_consumer.dart';
+export 'src/kafka_consumer.dart'; 
 
-// TODO: Export any libraries intended for clients of this package.
+// Models
+export 'src/models/export_models.dart';
