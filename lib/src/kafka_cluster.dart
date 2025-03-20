bd1eb93647de9d6882299f3bb6fb6efca39965bf
Eduardo Horn Cicchella <euardo.cic.horn@gmail.com>
2025-03-19 22:20:02 -0300

    Creation of the KafkaCluster class

---
+++ lib/src/kafka_cluster.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:dart_kafka/dart_kafka.dart';
+
+class KafkaCluster {
+  final Set<Broker> _brokers = {};
+  final Set<KafkaTopicMetadata> _topics = {};
+  final HashMap<String, Socket> _sockets = HashMap();
+  final Set<StreamSubscription> _subscriptions = {};
+
+  void addBroker(Broker broker) {
+    _brokers.add(broker);
+  }
+
+  void setBrokers(List<Broker> brokers) {
+    _brokers.clear();
+    _brokers.addAll(brokers);
+  }
+
+  void addTopic(KafkaTopicMetadata topic) {
+    _topics.add(topic);
+  }
+
+  void setTopics(List<KafkaTopicMetadata> topics) {
+    _topics.clear();
+    _topics.addAll(topics);
+  }
+
+  Socket getBrokerById(int id) {
+    if (!_sockets.containsKey(id)) {
+      throw Exception("Requested Broker not found!");
+    }
+    return _sockets[id]!;
+  }
+
+  Future<void> closeBroker({required brokerId}) async {
+    if (!_sockets.containsKey(brokerId)) {
+      throw Exception("Requested Broker not found!");
+    }
+
+    await _sockets[brokerId]!.close();
+    _sockets.remove(brokerId);
+  }
+
+  Future<void> connect({required Function responseHanddler}) async {
+    _brokers.map(
+      (Broker broker) async {
+        try {
+          String key = "${broker.host}:${broker.port}";
+          if (!_sockets.containsKey(key)) {
+            Socket sock = await Socket.connect(broker.host, broker.port);
+            _sockets.addAll({key: sock});
+
+            Future.microtask(() {
+              StreamSubscription subscription = sock.listen((event) => responseHanddler(event),);
+              _subscriptions.add(subscription);
+            }); 
+            
+          }
+        } catch (e) {
+          throw Exception("Error trying to connect to informed host: $e");
+        }
+      },
+    );
+  }
+}
