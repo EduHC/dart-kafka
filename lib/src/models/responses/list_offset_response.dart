fe42ec311d849fe49027046a901d445ddc3c6820
Eduardo Horn <eduardohorn@smartbr.com>
2025-07-24 14:08:36 -0300

    Hotfix: creating map/json methods in the ListOffsetResponse entity.

--- lib/src/models/responses/list_offset_response.dart
+++ lib/src/models/responses/list_offset_response.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dart_kafka/dart_kafka.dart';
 
 class ListOffsetResponse {
@@ -10,4 +12,31 @@ class ListOffsetResponse {
   String toString() {
     return "ListOffsetResponse -> throttleTimeMs: $throttleTimeMs, topics: $topics";
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'throttleTimeMs': throttleTimeMs,
+      'topics': topics
+          .map(
+            (e) => e.toMap(),
+          )
+          .toList()
+    };
+  }
+
+  factory ListOffsetResponse.fromMap(Map<String, dynamic> map) {
+    return ListOffsetResponse(
+      throttleTimeMs: map['throttleTimeMs'],
+      topics: (map['topics'] as List<dynamic>)
+          .map(
+            (e) => Topic.fromMap(e),
+          )
+          .toList(),
+    );
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory ListOffsetResponse.fromJson(String source) =>
+      ListOffsetResponse.fromMap(jsonDecode(source));
 }
